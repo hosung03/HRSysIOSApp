3c56ec2b9e568365228c58bb0a5159d51c859bd2
mac <mac@macui-MacBook-Pro.local>
2016-12-01 09:03:28 -0500

    Initial Commit

---
+++ HRSysIOSApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HRSysIOSApp
+//
+//  Created by mac on 2016. 12. 1..
+//  Copyright © 2016년 hosung. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
